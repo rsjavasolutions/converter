08b902d296cdd59f2263afc0c4c8583682369fa2
rsjavasolutions <stan.robert.1986@gmail.com>
2020-05-25 21:52:36 +0200

    Finished method returned all currencies

---
+++ src/main/java/com/rsjava/converter/controller/CurrencyController.java
@@ -0,0 +1,31 @@
+package com.rsjava.converter.controller;
+
+import com.rsjava.converter.dto.CurrencyDto;
+import com.rsjava.converter.dto.mapper.CurrencyMapper;
+import com.rsjava.converter.model.Rate;
+import com.rsjava.converter.service.CurrencyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/")
+public class CurrencyController {
+
+    private CurrencyService currencyService;
+    private CurrencyMapper currencyMapper;
+
+    @Autowired
+    public CurrencyController(CurrencyService currencyService, CurrencyMapper currencyMapper) {
+        this.currencyService = currencyService;
+        this.currencyMapper = currencyMapper;
+    }
+
+    @GetMapping("currencies")
+    public List<CurrencyDto> getCurrencies(){
+        return currencyMapper.mapToListCurrencyDto(currencyService.getAllRates());
+    }
+}
--- src/main/java/com/rsjava/converter/dto/mapper/CurrencyMapper.java
+++ src/main/java/com/rsjava/converter/dto/mapper/CurrencyMapper.java
@@ -4,23 +4,32 @@ import com.rsjava.converter.dto.CurrencyDto;
 import com.rsjava.converter.model.Rate;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.springframework.stereotype.Component;
 
 import java.util.Currency;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Component
 public class CurrencyMapper {
 
     public CurrencyDto mapToCurrencyDto(Rate rate){
         return new CurrencyDto(
                 rate.getCode(),
                 rate.getCurrency(),
-                rate.getMid().doubleValue());
+                getRoundedNumberToFourDecimalPlaces(rate.getMid().doubleValue()));
     }
 
     public List<CurrencyDto> mapToListCurrencyDto(List<Rate> rateList){
         return rateList.stream()
                 .map(currency -> mapToCurrencyDto(currency))
                 .collect(Collectors.toList());
+    }
+
+    private double getRoundedNumberToFourDecimalPlaces(double value){
+        value *= 10000;
+        value = Math.round(value);
+        value /= 10000;
+        return value;
     }
 }
