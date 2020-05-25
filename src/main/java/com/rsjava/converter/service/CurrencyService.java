2283de25455557b0e4212890bfc8aad42ac7eeba
rsjavasolutions <stan.robert.1986@gmail.com>
2020-05-25 15:09:17 +0200

    Added lombok library to project

---
+++ src/main/java/com/rsjava/converter/service/CurrencyService.java
@@ -0,0 +1,15 @@
+package com.rsjava.converter.service;
+
+import org.springframework.stereotype.Service;
+
+import java.net.URL;
+
+@Service
+public class CurrencyService {
+
+
+    public List<> getAllCurrencies(){
+        URL url = new URL()
+    }
+
+}
