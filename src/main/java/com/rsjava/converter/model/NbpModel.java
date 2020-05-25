00fff04451b2ba162f754c32b8e6da04ce522344
rsjavasolutions <stan.robert.1986@gmail.com>
2020-05-25 14:55:03 +0200

    Added 2 model classes, generated by jsonschema2pojo.org website

---
+++ src/main/java/com/rsjava/converter/model/NbpModel.java
@@ -0,0 +1,55 @@
+package com.rsjava.converter.model;
+
+
+import java.util.List;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class NbpModel {
+
+    @SerializedName("table")
+    @Expose
+    private String table;
+    @SerializedName("no")
+    @Expose
+    private String no;
+    @SerializedName("effectiveDate")
+    @Expose
+    private String effectiveDate;
+    @SerializedName("rates")
+    @Expose
+    private List<Rate> rates = null;
+
+    public String getTable() {
+        return table;
+    }
+
+    public void setTable(String table) {
+        this.table = table;
+    }
+
+    public String getNo() {
+        return no;
+    }
+
+    public void setNo(String no) {
+        this.no = no;
+    }
+
+    public String getEffectiveDate() {
+        return effectiveDate;
+    }
+
+    public void setEffectiveDate(String effectiveDate) {
+        this.effectiveDate = effectiveDate;
+    }
+
+    public List<Rate> getRates() {
+        return rates;
+    }
+
+    public void setRates(List<Rate> rates) {
+        this.rates = rates;
+    }
+
+}
