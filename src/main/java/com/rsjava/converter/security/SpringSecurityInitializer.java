11b8084337880c7e49d71b67f3a622c4d85d5b75
rsjavasolutions <stan.robert.1986@gmail.com>
2020-05-27 23:31:37 +0200

    All logic implemented: secure with api key every endpoints start wiht /api/ by API KEY

---
+++ src/main/java/com/rsjava/converter/security/SpringSecurityInitializer.java
@@ -0,0 +1,6 @@
+package com.rsjava.converter.security;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SpringSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+}
